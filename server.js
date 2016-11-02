95d29f015b4b5706e5340ff1c9603a9550c33787
狼族小狈 <1340641314@qq.com>
2016-11-02 14:10:55 +0800

    显示颜色

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var compiler = Webpack(webpackConfig)
 var server = new WebpackDevServer(compiler, {
     publicPath: config.publicPath,
     stats: {
-        colors: require('supports-color') //显示不同的颜色区分打包的文件
+        colors: true //显示不同的颜色区分打包的文件
     },
     proxy: { //代理服务器
         '*': {
