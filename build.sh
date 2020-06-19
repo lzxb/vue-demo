5cfd614e71cc02224fb379bb1d303c87b03d63db
lzxb <1340641314@qq.com>
2020-06-19 17:42:17 +0800

    fix(#21):修复编译没有copy html的bug

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 rm -rf node_modules
 yarn install --registry=https://registry.npm.taobao.org
 npm run build:ts
+cp index.html ./dist/node
 NODE_ENV=production node dist/node/genesis.build.js
 rm -rf node_modules
 yarn --prod --registry=https://registry.npm.taobao.org
