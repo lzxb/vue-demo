0fbe5b16afe98edbfb7ecac287db0698b21786a6
lzxb <1340641314@qq.com>
2022-08-25 10:55:55 +0800

    chore: 更新依赖

--- src/app.vue
+++ src/app.vue
@@ -3,6 +3,11 @@
         <router-view />
     </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
 <style lang="less" scoped>
 .app {
     max-width: 750px;
